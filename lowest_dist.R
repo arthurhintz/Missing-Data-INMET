b6e26d2d9ca4b985ebd48e301e5bd4032cffdc57
Arthur Hintz <114536155+arthurhintz@users.noreply.github.com>
2025-05-22 14:05:08 -0300

    Add files via upload
    I corrected some functions such as the max of the maximum temperature and the min of the minimum temperature, in addition to removing some stations that had no data in 2025. In addition to assigning a value of 0 to the negative values ​​of Radiation.

--- lowest_dist.R
+++ lowest_dist.R
@@ -1,67 +1,74 @@
+# Não tem dados em TOrres esse ano de 2025
+
 rm(list = ls())
 # First, I selected just the estations nearest 
 library(geosphere)
+library(tidyverse)
 
 estacoes <- read.csv2("estacoesRS.csv")
 
-estacoes$VL_LONGITUDE <- as.numeric(gsub(",", ".", estacoes$VL_LONGITUDE))
-estacoes$VL_LATITUDE <- as.numeric(gsub(",", ".", estacoes$VL_LATITUDE))
+#estacoes$VL_LONGITUDE <- as.numeric(gsub(",", ".", estacoes$VL_LONGITUDE))
+#estacoes$VL_LATITUDE <- as.numeric(gsub(",", ".", estacoes$VL_LATITUDE))
 
-dados <- as.matrix(estacoes[,c(4,3)])
+
+# Estou tirando Torres (40) pq não tem no ano de 2025 dados
+# Mostarda tbm não tem nenhum valor 
+# Filtrando estações inválidas
+estacoes_validas <- estacoes[!(estacoes$DC_NOME %in% c("TORRES", "MOSTARDAS")), ]
+idx_validos <- which(!(estacoes$DC_NOME %in% c("TORRES", "MOSTARDAS")))
+
+dados <- as.matrix(estacoes_validas[, c(4, 3)])
 
 distancias <- distm(dados, fun = distGeo)
 
-
 lowest_ind <- function(vetor) {
-  return(order(vetor)[1:3])
+  order(vetor)[2:3] 
 }
 
-
 ind_estac <- apply(distancias, 1, lowest_ind)
+
 
 print("Índices dos três menores valores por linha:")
 print(ind_estac)
 
-valor_dist <- apply(distancias, 1, function(x) sort(x)[1:3])
-
+valor_dist <- apply(distancias, 1, function(x) sort(x)[2:3])
 
 print("\nValores dos três menores por linha:")
 print(valor_dist)
 
-
-rm(valor_dist, dados, distancias)
-
-## agora preciso relacionar o indice com o número do cod da estacão
-library(tidyverse)
-
 ind_estac <- t(ind_estac)
 
-dataframe <- data.frame(coluna1 = ind_estac[,1],
-                        coluna2 = ind_estac[,2],
-                        coluna3 = ind_estac[,3])
 
+dataframe <- data.frame(
+  origem = idx_validos,  # índice original da estação de origem
+  coluna1 = idx_validos[ind_estac[,1]],
+  coluna2 = idx_validos[ind_estac[,2]]
+)
+
+# Parte da estação de origem
+parte_origem <- left_join(dataframe, estacoes, by = c("origem" = "X")) |> 
+  select(origem, DC_NOME_ORIGEM = DC_NOME, CD_ESTACAO_ORIGEM = CD_ESTACAO)
+
+# Parte da primeira vizinha
 parte1 <- left_join(dataframe, estacoes, by = c("coluna1" = "X")) |> 
-          select(coluna1, DC_NOME, CD_ESTACAO)
-  
+  select(DC_NOME_1 = DC_NOME, CD_ESTACAO_1 = CD_ESTACAO)
+
+# Parte da segunda vizinha
 parte2 <- left_join(dataframe, estacoes, by = c("coluna2" = "X")) |> 
-          select(coluna2, DC_NOME, CD_ESTACAO)
+  select(DC_NOME_2 = DC_NOME, CD_ESTACAO_2 = CD_ESTACAO)
 
-parte3 <- left_join(dataframe, estacoes, by = c("coluna3" = "X")) |> 
-          select(coluna3, DC_NOME, CD_ESTACAO)
-
-
-end <- cbind(parte1, parte2, parte3)
+# Combinando tudo
+end <- cbind(parte_origem, parte1, parte2)
 end
-
-
-rm(parte1, parte2, parte3, dataframe)
+# Limpando objetos
+rm(valor_dist, dados, distancias, parte_origem, parte2, parte1, dataframe)
 #____________________________________________________
 
 # Now I need to list and to fill the NAs
 
 library(imputeTS)
 
-names(end) <- c("coluna1", "NOME_EST_1" , "COD_EST_1", "coluna2", "NOME_EST_2" , "COD_EST_2", "coluna3", "NOME_EST_3" , "COD_EST_3")
+names(end) <- c("cod", "NOME_EST_1" , "COD_EST_1", "NOME_EST_2" , "COD_EST_2", "NOME_EST_3" , "COD_EST_3")
 
 
 
@@ -71,8 +78,8 @@ lista_vetor
 
 indices = seq_along(end$COD_EST_1)
 
-arq <- "_H_2019-06-01_2023-06-01.csv" 
-diretorio <- "dados_met_h/dados_"
+arq <- "_H_2024-06-01_2025-05-20.csv" 
+diretorio <- "inmet_24-25/dados_"
 
 for (j in indices){
   
@@ -81,6 +88,8 @@ for (j in indices){
   a = end$COD_EST_1[j]  
   b = end$COD_EST_2[j]
   c = end$COD_EST_3[j]  
+  
+  cat("EST 1:", a, "EST 2:", b, "EST 3:", c)
   
   
   arquivo1 <- paste(diretorio, a, arq, sep = "")  
@@ -133,6 +142,7 @@ for (j in indices){
   tudo <- left_join(data1, data2, by = c("timestemp"))
   tudo <- left_join(tudo, data3, by = c("timestemp"))
   
+  tudo_safe <- tudo
   
   # Verificando NAs
   # which(is.na(data1$Preciptacao_Total_h_1))
@@ -341,24 +351,79 @@ for (j in indices){
   last_data <- tudo |> 
     select(1:10) |> 
     group_by(Data_1) |> 
-    summarise(Temp_max = mean(Temp_max_1, na.rm = T),
-              Temp_min = mean(Temp_min_1, na.rm = T),
-              Media_vento = mean(Vel_Vento_1, na.rm = T),
-              Temp_AR_Bulbo = mean(Temp_AR_Bulbo_1, na.rm = T),
-              Temp_PO = mean(Temp_PO_1, na.rm = T),
-              Umidade_med = mean(Umidade_Relativa_1, na.rm = T),
-              Radiacao = sum(Radiacao_1, na.rm = T),
+    summarise(Temp_max = round(max(Temp_max_1, na.rm = T), 3),
+              Temp_min = round(min(Temp_min_1, na.rm = T), 3),
+              Media_vento = round(mean(Vel_Vento_1, na.rm = T), 3),
+              Temp_AR_Bulbo = round(mean(Temp_AR_Bulbo_1, na.rm = T), 3),
+              Temp_PO = round(mean(Temp_PO_1, na.rm = T), 3), 
+              Umidade_med = round(mean(Umidade_Relativa_1, na.rm = T), 3),
+              Radiacao = round(sum(ifelse(Radiacao_1 < 0, 0, Radiacao_1), na.rm = TRUE), 3),
               Preciptacao_total = sum(Preciptacao_Total_h_1, na.rm = T))
   
+  #_______________________Mexendo ainda_____Possivel erro_______
+ 
+  # Criar uma coluna indicadora para valores preenchidos
+png(file = paste("graficos/", a, ".png", sep = ""),  width = 600, height = 500)
+  {  
+  ver_data <- tudo_safe |> 
+    group_by(Data_1) |> 
+    summarise(Temp_max_1 = mean(Temp_max_1, na.rm = TRUE)) |> 
+    mutate(situacao = ifelse(!is.na(Temp_max_1), "Completo", "Missing"))
   
-  # Some plots
+  teste <- left_join(last_data, ver_data, by = c("Data_1")) |> 
+    select(c("Data_1", "Temp_max", "situacao"))
   
-  par(mfrow = c(2,2))
-  plot.ts(last_data$Temp_max)
-  plot.ts(last_data$Media_vento)
-  plot(last_data$Preciptacao_total)
-  plot.ts(last_data$Temp_min)
+#   # Plotar os valores
+#   plot(teste$Data_1[teste$situacao == "Completo"], teste$Temp_max[teste$situacao == "Completo"], 
+#        col = "blue",
+#        lwd = 1, lty = 1,
+#        type = "l",
+#        xlab = "Tempo", ylab = "Temperatura",
+#        ylim = range(c(data1$Temp_max_1, tudo$Temp_max_1), 
+#                     na.rm = TRUE))
+#   points(teste$Data_1[teste$situacao == "Missing"], teste$Temp_max[teste$situacao == "Missing"], col = "red", lwd = 1, lty = 8)
+#   legend("topright", legend = c("Completo", "Missing"), col = c("blue", "red"), lty = c(1,2), lwd = 2)
+# #___________________________________  
+#   # Plotar os valores
+  plot(teste$Data_1, y = teste$Temp_max, 
+       col = "blue",
+       type = "l", lwd = 2, lty = 1,
+       xlab = "Tempo", ylab = "Temperatura",
+       ylim = range(c(data1$Temp_max_1, tudo$Temp_max_1), na.rm = TRUE))
   
+  # Adicionar pontos coloridos para destacar os valores "Missing"
+  points(teste$Data_1[teste$situacao == "Missing"], teste$Temp_max[teste$situacao == "Missing"],
+         col = "red", pch = 16, cex = 1, type = "p")
+  
+  
+  # Adicionar legendas específicas para as linhas "Completo" e "Missing"
+  legend("topright", legend = c("Completo", "Missing"), col = c("blue", "red"), lty = 1, lwd = 2)
+  
+
+  
+ }
+dev.off()
+  
+    # Some plots
+  
+  
+  # par(mfrow = c(2,2))
+  # plot.ts(last_data$Temp_max)
+  # plot.ts(last_data$Media_vento)
+  # plot(last_data$Preciptacao_total)
+  # plot.ts(last_data$Temp_min)
+
+print(a)
+
+# Exemplo para temperatura máxima
+rmse <- sqrt(mean((tudo$Temp_max_1 - data1$Temp_max_1)^2, na.rm = TRUE))
+
+if(rmse != 0){
+  
+  cat("O valor RMSE =", rmse)
+}
+
+
 last_data$cod <- a
   
 write.csv2(last_data,  paste("ajust_met/",lista_vetor[j],".csv", sep = ""))
